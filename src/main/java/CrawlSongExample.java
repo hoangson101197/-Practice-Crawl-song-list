3ee7343fb412dce2a84a8af6331775e842ac30b5
= <=>
2018-09-27 09:13:46 +0700

    [Practice] Crawl song list

---
+++ src/main/java/CrawlSongExample.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CrawlSongExample {
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("https://www.nhaccuatui.com/bai-hat/bai-hat-moi.html");
+
+            //open the stream and put it into BufferedReader
+            Scanner sc = new Scanner(new InputStreamReader(url.openStream()));
+            sc.useDelimiter("\\Z");
+            String content = sc.next();
+            //close scanner
+            sc.close();
+
+            //remove all new line
+            content = content.replaceAll("\\n+", "");
+
+            //regex
+            Pattern p = Pattern.compile("name_song\">(.*?)</a>");
+            Matcher m = p.matcher(content);
+            while (m.find()) {
+                System.out.println(m.group(1));
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
